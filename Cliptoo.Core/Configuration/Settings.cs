99a964b4e65f1703e6365b6275fa10043bbf0377
dcog989 <sirdavidlaws@gmail.com>
2025-09-03 11:52:18 +0100

    settings #regions

--- Cliptoo.Core/Configuration/Settings.cs
+++ Cliptoo.Core/Configuration/Settings.cs
@@ -1,6 +1,6 @@
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Collections.Generic;
 
 namespace Cliptoo.Core.Configuration
 {
@@ -60,33 +60,14 @@ namespace Cliptoo.Core.Configuration
             return true;
         }
 
+        #region Appearance & Theming
         private string _theme = AppConstants.ThemeNames.System;
         [DefaultValue(AppConstants.ThemeNames.System)]
         public string Theme { get => _theme; set => SetProperty(ref _theme, value); }
 
-        private string _compareToolPath = "";
-        [DefaultValue("")]
-        public string CompareToolPath { get => _compareToolPath; set => SetProperty(ref _compareToolPath, value); }
-
-        private string _hotkey = "Ctrl+Alt+Q";
-        [DefaultValue("Ctrl+Alt+Q")]
-        public string Hotkey { get => _hotkey; set => SetProperty(ref _hotkey, value); }
-
         private string _accentColor = "#007ACC";
         [DefaultValue("#007ACC")]
         public string AccentColor { get => _accentColor; set => SetProperty(ref _accentColor, value); }
-
-        private string _launchPosition = "cursor";
-        [DefaultValue("cursor")]
-        public string LaunchPosition { get => _launchPosition; set => SetProperty(ref _launchPosition, value); }
-
-        private uint _cleanupAgeDays = 21;
-        [DefaultValue(21)]
-        public uint CleanupAgeDays { get => _cleanupAgeDays; set => SetProperty(ref _cleanupAgeDays, value); }
-
-        private uint _maxClipsTotal = 999;
-        [DefaultValue(999)]
-        public uint MaxClipsTotal { get => _maxClipsTotal; set => SetProperty(ref _maxClipsTotal, value); }
 
         private string _fontFamily = "Source Code Pro";
         [DefaultValue("Source Code Pro")]
@@ -95,6 +76,24 @@ namespace Cliptoo.Core.Configuration
         private float _fontSize = 14.0f;
         [DefaultValue(14.0f)]
         public float FontSize { get => _fontSize; set => SetProperty(ref _fontSize, value); }
+
+        private string _clipItemPadding = "standard";
+        [DefaultValue("standard")]
+        public string ClipItemPadding { get => _clipItemPadding; set => SetProperty(ref _clipItemPadding, value); }
+
+        private string _accentChromaLevel = "vibrant";
+        [DefaultValue("vibrant")]
+        public string AccentChromaLevel { get => _accentChromaLevel; set => SetProperty(ref _accentChromaLevel, value); }
+
+        private bool _displayLogo = true;
+        [DefaultValue(true)]
+        public bool DisplayLogo { get => _displayLogo; set => SetProperty(ref _displayLogo, value); }
+        #endregion
+
+        #region Window Management
+        private string _launchPosition = "cursor";
+        [DefaultValue("cursor")]
+        public string LaunchPosition { get => _launchPosition; set => SetProperty(ref _launchPosition, value); }
 
         private double _windowWidth = 400.0;
         [DefaultValue(400.0)]
@@ -112,65 +111,9 @@ namespace Cliptoo.Core.Configuration
         [DefaultValue(100)]
         public int FixedY { get => _fixedY; set => SetProperty(ref _fixedY, value); }
 
-        private string _clipItemPadding = "standard";
-        [DefaultValue("standard")]
-        public string ClipItemPadding { get => _clipItemPadding; set => SetProperty(ref _clipItemPadding, value); }
-
-        private string _accentChromaLevel = "vibrant";
-        [DefaultValue("vibrant")]
-        public string AccentChromaLevel { get => _accentChromaLevel; set => SetProperty(ref _accentChromaLevel, value); }
-
-        private bool _displayLogo = true;
-        [DefaultValue(true)]
-        public bool DisplayLogo { get => _displayLogo; set => SetProperty(ref _displayLogo, value); }
-
-        private string _loggingLevel = "None";
-        [DefaultValue("None")]
-        public string LoggingLevel { get => _loggingLevel; set => SetProperty(ref _loggingLevel, value); }
-
-        private bool _isAlwaysOnTop = false;
+        private bool _isAlwaysOnTop;
         [DefaultValue(false)]
         public bool IsAlwaysOnTop { get => _isAlwaysOnTop; set => SetProperty(ref _isAlwaysOnTop, value); }
-
-        private bool _showHoverPreview = true;
-        [DefaultValue(true)]
-        public bool ShowHoverPreview { get => _showHoverPreview; set => SetProperty(ref _showHoverPreview, value); }
-
-        private uint _hoverPreviewDelay = 1250;
-        [DefaultValue(1250)]
-        public uint HoverPreviewDelay { get => _hoverPreviewDelay; set => SetProperty(ref _hoverPreviewDelay, value); }
-
-        private uint _hoverImagePreviewSize = 350;
-        [DefaultValue(350)]
-        public uint HoverImagePreviewSize { get => _hoverImagePreviewSize; set => SetProperty(ref _hoverImagePreviewSize, value); }
-
-        private string _previewHotkey = "F3";
-        [DefaultValue("F3")]
-        public string PreviewHotkey { get => _previewHotkey; set => SetProperty(ref _previewHotkey, value); }
-
-        private string _quickPasteHotkey = "Ctrl+Alt";
-        [DefaultValue("Ctrl+Alt")]
-        public string QuickPasteHotkey { get => _quickPasteHotkey; set => SetProperty(ref _quickPasteHotkey, value); }
-
-        private uint _previewTooltipMaxWidth = 600;
-        [DefaultValue(600)]
-        public uint PreviewTooltipMaxWidth { get => _previewTooltipMaxWidth; set => SetProperty(ref _previewTooltipMaxWidth, value); }
-
-        private uint _maxClipSizeMb = 100;
-        [DefaultValue(100)]
-        public uint MaxClipSizeMb { get => _maxClipSizeMb; set => SetProperty(ref _maxClipSizeMb, value); }
-
-        private bool _startWithWindows = false;
-        [DefaultValue(false)]
-        public bool StartWithWindows { get => _startWithWindows; set => SetProperty(ref _startWithWindows, value); }
-
-        private string _previewFontFamily = "Source Code Pro";
-        [DefaultValue("Source Code Pro")]
-        public string PreviewFontFamily { get => _previewFontFamily; set => SetProperty(ref _previewFontFamily, value); }
-
-        private float _previewFontSize = 14.0f;
-        [DefaultValue(14.0f)]
-        public float PreviewFontSize { get => _previewFontSize; set => SetProperty(ref _previewFontSize, value); }
 
         private double _editorWindowWidth = 600.0;
         [DefaultValue(600.0)]
@@ -188,18 +131,6 @@ namespace Cliptoo.Core.Configuration
         [DefaultValue(-1.0)]
         public double EditorWindowY { get => _editorWindowY; set => SetProperty(ref _editorWindowY, value); }
 
-        private bool _rememberSearchInput = false;
-        [DefaultValue(false)]
-        public bool RememberSearchInput { get => _rememberSearchInput; set => SetProperty(ref _rememberSearchInput, value); }
-
-        private bool _rememberFilterSelection = false;
-        [DefaultValue(false)]
-        public bool RememberFilterSelection { get => _rememberFilterSelection; set => SetProperty(ref _rememberFilterSelection, value); }
-
-        private bool _pasteAsPlainText = false;
-        [DefaultValue(false)]
-        public bool PasteAsPlainText { get => _pasteAsPlainText; set => SetProperty(ref _pasteAsPlainText, value); }
-
         private double _settingsWindowWidth = 800.0;
         [DefaultValue(800.0)]
         public double SettingsWindowWidth { get => _settingsWindowWidth; set => SetProperty(ref _settingsWindowWidth, value); }
@@ -215,6 +146,90 @@ namespace Cliptoo.Core.Configuration
         private double _settingsWindowY = -1.0;
         [DefaultValue(-1.0)]
         public double SettingsWindowY { get => _settingsWindowY; set => SetProperty(ref _settingsWindowY, value); }
-        public List<SendToTarget> SendToTargets { get; set; } = new();
+        #endregion
+
+        #region Hotkeys
+        private string _hotkey = "Ctrl+Alt+Q";
+        [DefaultValue("Ctrl+Alt+Q")]
+        public string Hotkey { get => _hotkey; set => SetProperty(ref _hotkey, value); }
+
+        private string _previewHotkey = "F3";
+        [DefaultValue("F3")]
+        public string PreviewHotkey { get => _previewHotkey; set => SetProperty(ref _previewHotkey, value); }
+
+        private string _quickPasteHotkey = "Ctrl+Alt";
+        [DefaultValue("Ctrl+Alt")]
+        public string QuickPasteHotkey { get => _quickPasteHotkey; set => SetProperty(ref _quickPasteHotkey, value); }
+        #endregion
+
+        #region Preview & Tooltip
+        private bool _showHoverPreview = true;
+        [DefaultValue(true)]
+        public bool ShowHoverPreview { get => _showHoverPreview; set => SetProperty(ref _showHoverPreview, value); }
+
+        private uint _hoverPreviewDelay = 1250;
+        [DefaultValue(1250)]
+        public uint HoverPreviewDelay { get => _hoverPreviewDelay; set => SetProperty(ref _hoverPreviewDelay, value); }
+
+        private uint _hoverImagePreviewSize = 350;
+        [DefaultValue(350)]
+        public uint HoverImagePreviewSize { get => _hoverImagePreviewSize; set => SetProperty(ref _hoverImagePreviewSize, value); }
+
+        private uint _previewTooltipMaxWidth = 600;
+        [DefaultValue(600)]
+        public uint PreviewTooltipMaxWidth { get => _previewTooltipMaxWidth; set => SetProperty(ref _previewTooltipMaxWidth, value); }
+
+        private string _previewFontFamily = "Source Code Pro";
+        [DefaultValue("Source Code Pro")]
+        public string PreviewFontFamily { get => _previewFontFamily; set => SetProperty(ref _previewFontFamily, value); }
+
+        private float _previewFontSize = 14.0f;
+        [DefaultValue(14.0f)]
+        public float PreviewFontSize { get => _previewFontSize; set => SetProperty(ref _previewFontSize, value); }
+        #endregion
+
+        #region Data Management
+        private uint _cleanupAgeDays = 21;
+        [DefaultValue(21)]
+        public uint CleanupAgeDays { get => _cleanupAgeDays; set => SetProperty(ref _cleanupAgeDays, value); }
+
+        private uint _maxClipsTotal = 999;
+        [DefaultValue(999)]
+        public uint MaxClipsTotal { get => _maxClipsTotal; set => SetProperty(ref _maxClipsTotal, value); }
+
+        private uint _maxClipSizeMb = 100;
+        [DefaultValue(100)]
+        public uint MaxClipSizeMb { get => _maxClipSizeMb; set => SetProperty(ref _maxClipSizeMb, value); }
+        #endregion
+
+        #region Behavior & Functionality
+        private string _compareToolPath = "";
+        [DefaultValue("")]
+        public string CompareToolPath { get => _compareToolPath; set => SetProperty(ref _compareToolPath, value); }
+
+        private bool _startWithWindows;
+        [DefaultValue(false)]
+        public bool StartWithWindows { get => _startWithWindows; set => SetProperty(ref _startWithWindows, value); }
+
+        private bool _rememberSearchInput;
+        [DefaultValue(false)]
+        public bool RememberSearchInput { get => _rememberSearchInput; set => SetProperty(ref _rememberSearchInput, value); }
+
+        private bool _rememberFilterSelection;
+        [DefaultValue(false)]
+        public bool RememberFilterSelection { get => _rememberFilterSelection; set => SetProperty(ref _rememberFilterSelection, value); }
+
+        private bool _pasteAsPlainText;
+        [DefaultValue(false)]
+        public bool PasteAsPlainText { get => _pasteAsPlainText; set => SetProperty(ref _pasteAsPlainText, value); }
+
+        public Collection<SendToTarget> SendToTargets { get; } = new();
+        #endregion
+
+        #region Diagnostics
+        private string _loggingLevel = "None";
+        [DefaultValue("None")]
+        public string LoggingLevel { get => _loggingLevel; set => SetProperty(ref _loggingLevel, value); }
+        #endregion
     }
 }
