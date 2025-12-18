d58e089f2ae0ffa13fb2b3989b3f90332dd3a987
dcog989 <sirdavidlaws@gmail.com>
2025-12-18 12:52:54 +0000

    thumbnail loading faster

--- Cliptoo.UI/Controls/ClipItemControl.xaml.cs
+++ Cliptoo.UI/Controls/ClipItemControl.xaml.cs
@@ -1,33 +1,57 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 using Cliptoo.UI.ViewModels;
 
 namespace Cliptoo.UI.Controls
 {
     public partial class ClipItemControl : UserControl
     {
+        private readonly DispatcherTimer _loadTimer;
+
         public ClipItemControl()
         {
             InitializeComponent();
+
+            _loadTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(125) };
+            _loadTimer.Tick += OnLoadTimerTick;
+
             DataContextChanged += OnDataContextChanged;
             Unloaded += OnUnloaded;
         }
 
         private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+            _loadTimer.Stop();
+
             if (e.OldValue is ClipViewModel oldVm)
             {
                 oldVm.ReleaseThumbnail();
             }
+
             if (e.NewValue is ClipViewModel newVm)
             {
-                _ = newVm.LoadThumbnailAsync();
+                // Ensure the recycled control doesn't show the previous item's image
+                newVm.ReleaseThumbnail();
+
+                // Delay loading to ensure the user has stopped scrolling on this item
+                _loadTimer.Start();
+            }
+        }
+
+        private void OnLoadTimerTick(object? sender, EventArgs e)
+        {
+            _loadTimer.Stop();
+            if (DataContext is ClipViewModel vm)
+            {
+                _ = vm.LoadThumbnailAsync();
             }
         }
 
         private void OnUnloaded(object sender, RoutedEventArgs e)
         {
+            _loadTimer.Stop();
             if (DataContext is ClipViewModel vm)
             {
                 vm.ReleaseThumbnail();
@@ -50,6 +74,5 @@ namespace Cliptoo.UI.Controls
                 clipVm.RequestHidePreview();
             }
         }
-
     }
 }
