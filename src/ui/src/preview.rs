1ae09e6718abfcbc0870efa330026220eafe9d79
dcog989 <sirdavidlaws@gmail.com>
2026-07-06 13:45:59 +0100

    use chrono, not hand roll

--- src/ui/src/preview.rs
+++ src/ui/src/preview.rs
@@ -201,25 +201,9 @@ pub fn setup_preview(
                                     };
                                     let item_label = if count == 1 { "item" } else { "items" };
                                     let date_str = if latest_mtime > 0 {
-                                        let secs = latest_mtime;
-                                        let days = secs / 86400;
-                                        let time_secs = secs % 86400;
-                                        let hours = time_secs / 3600;
-                                        let minutes = (time_secs % 3600) / 60;
-                                        let z = days + 719468;
-                                        let era = if z >= 0 { z } else { z - 146096 } / 146097;
-                                        let doe = z - era * 146097;
-                                        let yoe =
-                                            (doe - doe / 1460 + doe / 36524 - doe / 146096) / 365;
-                                        let y = yoe + era * 400;
-                                        let doy = doe - (365 * yoe + yoe / 4 - yoe / 100);
-                                        let mp = (5 * doy + 2) / 153;
-                                        let d = doy - (153 * mp + 2) / 5 + 1;
-                                        let m = if mp < 10 { mp + 3 } else { mp - 9 };
-                                        format!(
-                                            "{:04}-{:02}-{:02} {:02}:{:02}",
-                                            y, m, d, hours, minutes
-                                        )
+                                        chrono::DateTime::from_timestamp(latest_mtime, 0)
+                                            .map(|dt| dt.format("%Y-%m-%d %H:%M").to_string())
+                                            .unwrap_or_default()
                                     } else {
                                         String::new()
                                     };
